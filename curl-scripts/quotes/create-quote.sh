ac4dcb7fa0397da56ed4b2ac73d154a1dc6ec675
sars604 <sars604@gmail.com>
2019-01-30 13:05:20 -0500

    Adjust controller to be OpenReadController
    - Able to create, update, destroy quotes created by a user
    - Unable to edit other users quotes

--- curl-scripts/quotes/create-quote.sh
+++ curl-scripts/quotes/create-quote.sh
@@ -4,6 +4,7 @@ curl "http://localhost:4741/quotes" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "quote": {
       "text": "'"${TEXT}"'",
